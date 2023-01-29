cd659630087e3136ce09bdafb2b99dd575179bce
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-01-29 23:24:17 +0100

    Update home.dart

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -3,3 +3,12 @@ import 'package:bmi/widgets/left.dart';
 import 'package:bmi/widgets/right.dart';
 import 'package:flutter/material.dart';
 import '../const/app_const.dart';
+
+// ignore: camel_case_types
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+_HomeScreen createState() => _HomeScreen();
+}
+
