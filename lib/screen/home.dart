db5b56c8540fb0a52ba823a3710d8b8eecc9a32c
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-01-29 23:25:36 +0100

    Update home.dart

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -62,3 +62,21 @@ class _HomeScreen extends State<HomeScreen> {
                 
             ]
           ),
+          SizedBox(height: 30,),
+          Container(
+            child: Text("Calculate", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: accentHexColor)),
+          ),
+          SizedBox(height: 50,),
+          Container(
+            child: Text("10", style: TextStyle(fontSize: 90, color: accentHexColor)),
+          ),
+          SizedBox(height: 30,),
+          Container(
+            child: Text("Normal weight", style: TextStyle(fontSize: 32, fontWeight: FontWeight.w400, color: accentHexColor)),
+          ),
+
+        ],
+      )),
+    );
+  }
+}
